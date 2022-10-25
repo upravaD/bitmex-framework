83d5fb9c0c32a0476c6b1703c9c9c6370ef7f2b9
upravaD <daktah@icloud.com>
2022-10-25 16:31:02 +0400

    new user commit

--- src/main/java/org/example/framework/App.java
+++ src/main/java/org/example/framework/App.java
@@ -1,18 +1,19 @@
 package org.example.framework;
 
-import java.net.http.HttpResponse;
 import org.example.framework.constants.OrderSide;
 import org.example.framework.constants.Symbol;
 import org.example.framework.order.LimitOrder;
 
+import java.net.http.HttpResponse;
+
 public class App {
 
   public static void main(String[] args) {
-    String apiKey = "LDqZ9I7HO41M-U3t4ke583x5";
-    String apiSecret = "IXdZW9wP3za2XXwmYIs1ah3E-5JwD4RQdWtOvtgHDALBYp5J";
+    String apiKey = "B0JgUAGtaNYrAxttArJT-v5H";
+    String apiSecret = "ztEAx9hQf84BtF3Mc0VtaSWThqfqpyXxetHniIGAP_H0HAyv";
     BitmexClient bitmexClient = new BitmexClient(apiKey, apiSecret, true);
 
-    LimitOrder order = new LimitOrder(Symbol.XBTUSD, OrderSide.SELL, 30000.0, 100.0);
+    LimitOrder order = new LimitOrder(Symbol.XBTUSD, OrderSide.SELL, 3000.0, 10.0);
 
     HttpResponse<String> response = bitmexClient.sendOrder(order);
     System.out.println(response.body());
